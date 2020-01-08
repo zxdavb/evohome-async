0f2568ccfec579476088c6912f7de0d26700c1c5
David Bonnes <zxdavb@bonnes.me>
2020-01-08 17:31:13 +0000

    fix async omission

--- evohomeasync2/controlsystem.py
+++ evohomeasync2/controlsystem.py
@@ -57,9 +57,9 @@ class ControlSystem(object):                                                    
         ) as response:
             response.raise_for_status()
 
-    def set_status(self, mode, until=None):
+    async def set_status(self, mode, until=None):
         """Set the system to a mode, either indefinitely, or for a set time."""
-        self._set_status(mode, until)
+        await self._set_status(mode, until)
 
     async def set_status_normal(self):
         """Set the system into normal mode."""
