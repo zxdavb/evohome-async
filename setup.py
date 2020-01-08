d9046f87f792f97d59888f5efa52d093deadad43
David Bonnes <zxdavb@bonnes.me>
2020-01-08 15:44:22 +0000

    0.3.5 - align with evohome-client

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import sys
 from setuptools import setup
 from setuptools.command.install import install
 
-VERSION = "0.3.4b2"
+VERSION = "0.3.5"
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
