b649a457b99050e7076888842c226e7d6920f18f
David Bonnes <zxdavb@bonnes.me>
2020-02-27 23:00:11 +0000

    fix gateway.py

--- evohomeasync2/gateway.py
+++ evohomeasync2/gateway.py
@@ -1,11 +1,11 @@
-"""Provides handling of a gateway."""
+"""Provide handling of a gateway."""
 from .controlsystem import ControlSystem
 
 
 class Gateway(
     object
 ):  # pylint: disable=too-few-public-methods,useless-object-inheritance
-    """Provides handling of a gateway."""
+    """Provide handling of a gateway."""
 
     def __init__(self, client, location, data=None):
         """Initialise the class."""
