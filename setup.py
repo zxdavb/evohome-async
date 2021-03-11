dbe83d01fba49fe1b4ce105d56a61b0a3dfd6683
David Bonnes <david@bonnes.me>
2021-03-11 00:39:49 +0000

    workflow

--- setup.py
+++ setup.py
@@ -1,54 +1,55 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+#
 """The setup.py file."""
 
 import os
-from setuptools import setup
-from setuptools.command.install import install
 import sys
 
-VERSION = "0.3.8"
-
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+from setuptools import find_packages, setup
+from setuptools.command.install import install
 
 
 class VerifyVersionCommand(install):
-    """Custom command to verify that the git tag matches our version."""
-
-    description = "verify that the git tag matches our version"
+    """Custom command to verify that the git tag matches our VERSION."""
 
     def run(self):
         tag = os.getenv("CIRCLE_TAG")
-
         if tag != VERSION:
-            info = "Git tag: {0} does not match the version of this app: {1}".format(
-                tag, VERSION
-            )
+            info = "Git tag: {tag} does not match the version of this pkg: {VERSION}"
             sys.exit(info)
+
+
+VERSION = "0.3.8"
+
+
+with open("README.md", "r") as fh:
+    LONG_DESCRIPTION = fh.read()
 
 
 setup(
     name="evohome-async",
-    version=VERSION,
+    description="An async client for connecting to Honeywell's TCC RESTful API.",
+    keywords=["evohome", "total connect comfort", "round thermostat"],
     author="Andrew Stock & David Bonnes",
     author_email="zxdavb@bonnes.me",
-    description="An async Python client for connecting to the Evohome webservice",
-    keywords=["evohome", "total connect comfort", "round thermostat"],
-    long_description=long_description,
+    url="https://github.com/zxdavb/evohome-async",
+    download_url="{url}/archive/{VERSION}.tar.gz",
+    install_requires=[list(val.strip() for val in open("requirements.txt"))],
+    long_description=LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
-    url="https://github.com/zxdavb/evohome-async/",
-    download_url="https://github.com/zxdavb/evohome-async/tarball/" + VERSION,
+    packages=find_packages(exclude=["test", "docs"]),
+    version=VERSION,
     license="Apache 2",
-    packages=["evohomeasync", "evohomeasync2"],
-    install_requires=["aiohttp"],
+    python_requires=">=3.7",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Home Automation",
+    ],
     cmdclass={
         "verify": VerifyVersionCommand,
     },
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: Apache Software License",
-        "Operating System :: OS Independent",
-        "Development Status :: 5 - Production/Stable",
-    ],
 )
