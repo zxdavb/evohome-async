ce82e9b02b8a5f6a99b806d39adffadd08e8504b
David Bonnes <zxdavb@bonnes.me>
2023-10-19 00:34:46 +0100

    lint

--- evohomeasync2/schema/__init__.py
+++ evohomeasync2/schema/__init__.py
@@ -4,14 +4,14 @@
 """evohomeasync2 - Schema for vendor's RESTful API JSON."""
 from __future__ import annotations
 
-try:  # noqa: ignore[F401]
+try:
     from .account import SCH_USER_ACCOUNT as SCH_USER_ACCOUNT
     from .account import SCH_OAUTH_TOKEN
     from .config import SCH_LOCATION_INSTALLATION_INFO as SCH_LOCN_CONFIG
     from .config import SCH_USER_LOCATIONS_INSTALLATION_INFO as SCH_FULL_CONFIG
-    from .status import SCH_TEMPERATURE_CONTROL_SYSTEM as SCH_TCS_STATUS
     from .status import SCH_DHW as SCH_DHW_STATUS
     from .status import SCH_LOCATION_STATUS as SCH_LOCN_STATUS
+    from .status import SCH_TEMPERATURE_CONTROL_SYSTEM as SCH_TCS_STATUS
     from .status import SCH_ZONE as SCH_ZONE_STATUS
 
 except ModuleNotFoundError:  # No module named 'voluptuous'
@@ -21,4 +21,5 @@ except ModuleNotFoundError:  # No module named 'voluptuous'
     SCH_FULL_CONFIG = list
     SCH_DHW_STATUS = dict
     SCH_LOCN_STATUS = dict
+    SCH_TCS_STATUS = dict
     SCH_ZONE_STATUS = dict
