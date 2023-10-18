8fbe6daacff54a89aa0d46d090b046c692b63869
David Bonnes <zxdavb@bonnes.me>
2023-10-18 11:22:16 +0100

    tweak schema so can survive no voluptuous

--- evohomeasync2/schema/__init__.py
+++ evohomeasync2/schema/__init__.py
@@ -1,13 +1,19 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 #
-"""evohomeasync2 - Schema for RESTful API Config JSON."""
+"""evohomeasync2 - Schema for vendor's RESTful API JSON."""
 from __future__ import annotations
 
-from .account import SCH_USER_ACCOUNT as SCH_USER_ACCOUNT  # noqa: ignore[F401]
-from .account import SCH_OAUTH_TOKEN  # noqa: ignore[F401]
-from .config import (  # noqa: ignore[F401]
-    SCH_LOCATION_INSTALLATION_INFO as SCH_LOCN_CONFIG,
-    SCH_USER_LOCATIONS_INSTALLATION_INFO as SCH_FULL_CONFIG,
-)
-from .status import SCH_LOCATION_STATUS as SCH_LOCN_STATUS  # noqa: ignore[F401]
+try:  # noqa: ignore[F401]
+    from .account import SCH_USER_ACCOUNT as SCH_USER_ACCOUNT
+    from .account import SCH_OAUTH_TOKEN
+    from .config import SCH_LOCATION_INSTALLATION_INFO as SCH_LOCN_CONFIG
+    from .config import SCH_USER_LOCATIONS_INSTALLATION_INFO as SCH_FULL_CONFIG
+    from .status import SCH_LOCATION_STATUS as SCH_LOCN_STATUS
+
+except ModuleNotFoundError:  # No module named 'voluptuous'
+    SCH_USER_ACCOUNT = dict
+    SCH_OAUTH_TOKEN = dict
+    SCH_LOCN_CONFIG = dict
+    SCH_FULL_CONFIG = list
+    SCH_LOCN_STATUS = dict
