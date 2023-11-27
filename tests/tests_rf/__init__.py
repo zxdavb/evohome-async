ead3fcd9ff31388555e6f0c705cb75447b38484b
David Bonnes <zxdavb@bonnes.me>
2023-11-27 17:12:46 +0000

    doctweak

--- tests/tests_rf/__init__.py
+++ tests/tests_rf/__init__.py
@@ -4,6 +4,6 @@
 """Mocked vendor server for provision via a hacked aiohttp."""
 from __future__ import annotations
 
-# normally, we want these flags to be False
+# normally, we want these debug flags to be False
 _DEBUG_USE_REAL_AIOHTTP = False
 _DEBUG_DISABLE_STRICT_ASSERTS = False  # of response content-type, schema
