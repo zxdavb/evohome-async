5e9e82a9722ed805a4ab3cbc9db5b36bc642cb79
David Bonnes <zxdavb@bonnes.me>
2024-04-20 10:45:08 +0100

    mypy

--- tests/tests_rf/mocked_server/__init__.py
+++ tests/tests_rf/mocked_server/__init__.py
@@ -5,6 +5,7 @@
 
 from __future__ import annotations
 
-from . import aiohttp  # noqa: F401
+# from . import aiohttp  # noqa: F401
+from .aiohttp import ClientSession  # noqa: F401
 from .const import GHOST_ZONE_ID  # noqa: F401
 from .vendor import MockedServer  # noqa: F401
