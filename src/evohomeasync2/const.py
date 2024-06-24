37b3415a397bfe8745d2ea007f54b2a34b4dd2f9
David Bonnes <zxdavb@bonnes.me>
2024-06-24 15:39:42 +0100

    typing

--- src/evohomeasync2/const.py
+++ src/evohomeasync2/const.py
@@ -5,32 +5,32 @@ from typing import Final
 
 from .schema import DhwState as DhwState, SystemMode as SystemMode, ZoneMode as ZoneMode
 
-URL_HOST = "https://tccna.honeywell.com"
+URL_HOST: Final = "https://tccna.honeywell.com"
 
-AUTH_URL = f"{URL_HOST}/Auth/OAuth/Token"
-URL_BASE = f"{URL_HOST}/WebAPI/emea/api/v1"
+AUTH_URL: Final = f"{URL_HOST}/Auth/OAuth/Token"
+URL_BASE: Final = f"{URL_HOST}/WebAPI/emea/api/v1"
 
-HDR_STRFTIME = "%Y-%m-%d %H:%M:%S"  # used by HTTP headers
-API_STRFTIME = "%Y-%m-%dT%H:%M:%SZ"  # used by API
+HDR_STRFTIME: Final = "%Y-%m-%d %H:%M:%S"  # used by HTTP headers
+API_STRFTIME: Final = "%Y-%m-%dT%H:%M:%SZ"  # used by API
 
-AUTH_HEADER_ACCEPT = (
+AUTH_HEADER_ACCEPT: Final = (
     "application/json, application/xml, "
     "text/json, text/x-json, "
     "text/javascript, text/xml"
 )
 
-_AUTH_HEADER_BASIC = (
+_AUTH_HEADER_BASIC: Final = (
     "Basic "
     "NGEyMzEwODktZDJiNi00MWJkLWE1ZWItMTZhMGE0MjJiOTk5OjFhMTVjZGI4LTQyZGUtNDA3Y"
     "i1hZGQwLTA1OWY5MmM1MzBjYg=="
 )
 
-AUTH_HEADER = {
+AUTH_HEADER: Final = {
     "Accept": AUTH_HEADER_ACCEPT,
     "Authorization": _AUTH_HEADER_BASIC,
 }
 
-AUTH_PAYLOAD = {
+AUTH_PAYLOAD: Final = {
     "Content-Type": "application/x-www-form-urlencoded; charset=utf-8",
     "Host": "rs.alarmnet.com/",
     "Cache-Control": "no-store no-cache",
@@ -38,13 +38,13 @@ AUTH_PAYLOAD = {
     "Connection": "Keep-Alive",
 }
 
-CREDS_REFRESH_TOKEN = {
+CREDS_REFRESH_TOKEN: Final = {
     "grant_type": "refresh_token",
     "scope": "EMEA-V1-Basic EMEA-V1-Anonymous",
     "refresh_token": None,
 }
 
-CREDS_USER_PASSWORD = {
+CREDS_USER_PASSWORD: Final = {
     "grant_type": "password",
     "scope": "EMEA-V1-Basic EMEA-V1-Anonymous EMEA-V1-Get-Current-User-Account",
     "Username": None,
