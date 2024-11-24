be5442abdb524e54983f6a32072b1c818fe802ab
David Bonnes <zxdavb@bonnes.me>
2024-11-24 12:36:06 +0000

    tweak test

--- tests/tests_rf/test_v2_urls_sched.py
+++ tests/tests_rf/test_v2_urls_sched.py
@@ -17,63 +17,141 @@ from typing import TYPE_CHECKING
 from evohomeasync2 import schemas
 
 from ..const import _DBG_USE_REAL_AIOHTTP
-from . import faked_server as faked
 from .common import should_fail_v2, should_work_v2, skipif_auth_failed
 
 if TYPE_CHECKING:
     from ..conftest import EvohomeClientv2
 
 
-async def _test_schedule(evo: EvohomeClientv2) -> None:
-    """Test /{x._TYPE}/{x.id}/schedule
+async def _test_schedule_put(evo: EvohomeClientv2) -> None:
+    """Test /{x._TYPE}/{x.id}/schedule"""
 
-    Also test commTasks?commTaskId={task_id}
-    """
-
-    schedule: dict[str, list]  # {'dailySchedules': [...]}
+    schedule: dict[str, list[dict[str, list[dict]]]]  # {'dailySchedules': [...]}
 
     # TODO: remove .update() and use URLs only
     await evo.update()
 
     zone = evo.locations[0].gateways[0].control_systems[0].zones[0]
-    #
-
-    if zone.id == faked.GHOST_ZONE_ID:
-        url = f"{zone._TYPE}/{faked.GHOST_ZONE_ID}/schedule"
-        _ = await should_fail_v2(
-            evo.auth, HTTPMethod.GET, url, status=HTTPStatus.BAD_REQUEST
-        )
-        return
-
-    #
-    # STEP 0: GET the current schedule
     url = f"{zone._TYPE}/{zone.id}/schedule"
+
+    #
+    # STEP 1: GET the current schedule
     schedule = await should_work_v2(
         evo.auth, HTTPMethod.GET, url, schema=schemas.SCH_GET_SCHEDULE
-    )  # {'dailySchedules': [...]}
+    )  # type: ignore[assignment]
+
+    # an example of the expected response:
+    """
+        {
+            'dailySchedules': [
+                {
+                    'dayOfWeek': 'Monday',
+                    'switchpoints': [
+                        {'heatSetpoint': 23.1, 'timeOfDay': '06:30:00'},
+                        {'heatSetpoint': 18.0, 'timeOfDay': '08:00:00'},
+                        {'heatSetpoint': 19.1, 'timeOfDay': '17:00:00'},
+                        {'heatSetpoint': 14.9, 'timeOfDay': '21:30:00'}
+                    ]
+                }, {
+                    'dayOfWeek': 'Tuesday',
+                    'switchpoints': [{'heatSetpoint': 19.1, 'timeOfDay': '06:30:00'}, {'heatSetpoint': 18.0, 'timeOfDay': '08:00:00'}, {'heatSetpoint': 19.1, 'timeOfDay': '17:00:00'}, {'heatSetpoint': 14.9, 'timeOfDay': '21:30:00'}]
+                },{
+                    'dayOfWeek': 'Wednesday',
+                    'switchpoints': [{'heatSetpoint': 19.1, 'timeOfDay': '06:30:00'}, {'heatSetpoint': 18.0, 'timeOfDay': '08:00:00'}, {'heatSetpoint': 19.1, 'timeOfDay': '17:00:00'}, {'heatSetpoint': 14.9, 'timeOfDay': '21:30:00'}]
+                }, {
+                    'dayOfWeek': 'Thursday',
+                    'switchpoints': [{'heatSetpoint': 19.1, 'timeOfDay': '06:30:00'}, {'heatSetpoint': 18.0, 'timeOfDay': '08:00:00'}, {'heatSetpoint': 19.1, 'timeOfDay': '17:00:00'}, {'heatSetpoint': 14.9, 'timeOfDay': '21:30:00'}]
+                }, {
+                    'dayOfWeek': 'Friday',
+                    'switchpoints': [{'heatSetpoint': 19.1, 'timeOfDay': '06:30:00'}, {'heatSetpoint': 18.0, 'timeOfDay': '08:00:00'}, {'heatSetpoint': 19.1, 'timeOfDay': '17:00:00'}, {'heatSetpoint': 14.9, 'timeOfDay': '21:30:00'}]
+                }, {
+                    'dayOfWeek': 'Saturday',
+                    'switchpoints': [{'heatSetpoint': 19.1, 'timeOfDay': '07:30:00'}, {'heatSetpoint': 18.5, 'timeOfDay': '11:00:00'}, {'heatSetpoint': 19.1, 'timeOfDay': '17:00:00'}, {'heatSetpoint': 14.9, 'timeOfDay': '21:30:00'}]
+                }, {
+                    'dayOfWeek': 'Sunday',
+                    'switchpoints': [{'heatSetpoint': 19.1, 'timeOfDay': '07:30:00'}, {'heatSetpoint': 18.5, 'timeOfDay': '11:00:00'}, {'heatSetpoint': 19.1, 'timeOfDay': '17:00:00'}, {'heatSetpoint': 14.9, 'timeOfDay': '21:30:00'}]
+                }
+            ]
+        }
+    """
 
     #
-    # STEP 1: PUT a new schedule
+    # STEP 2: PUT a null schedule  # NOTE: json=None
+    _ = await should_fail_v2(
+        evo.auth, HTTPMethod.PUT, url, json=None, status=HTTPStatus.BAD_REQUEST
+    )
+
+    # the expected response:
+    """
+        [{
+            'code': 'ParameterIsMissing',
+            'parameterName': 'request',
+            'message': 'Parameter is missing.'
+    }]
+    """
+
+    #
+    # STEP 3: PUT an invalid schedule  # NOTE: json={}
+    _ = await should_fail_v2(
+        evo.auth, HTTPMethod.PUT, url, json={}, status=HTTPStatus.BAD_REQUEST
+    )
+
+    # the expected response:
+    """
+        [{
+            'code': 'DayOfWeekMissing',
+            'message': 'One or more days missing.'
+        }]
+    """
+
+    #
+    # STEP 4: PUT a valid schedule
+    _ = await should_work_v2(evo.auth, HTTPMethod.PUT, url, json=schedule)
+
+    # an example of the expected response:
+    """
+        {'id': '840367013'}
+    """
+
+
+async def _test_schedule_tsk(evo: EvohomeClientv2) -> None:
+    """Test /{x._TYPE}/{x.id}/schedule
+
+    Also test commTasks?commTaskId={task_id}
+    """
+
+    schedule: dict[str, list[dict[str, list[dict]]]]  # {'dailySchedules': [...]}
+
+    # TODO: remove .update() and use URLs only
+    await evo.update()
+
+    zone = evo.locations[0].gateways[0].control_systems[0].zones[0]
+    url = f"{zone._TYPE}/{zone.id}/schedule"
+
+    #
+    # STEP 1: GET the current schedule
+    schedule = await should_work_v2(
+        evo.auth, HTTPMethod.GET, url, schema=schemas.SCH_GET_SCHEDULE
+    )  # type: ignore[assignment]
+
     if not _DBG_USE_REAL_AIOHTTP:  # TODO: faked server using old schema
         return
 
-    temp = schedule["dailySchedules"][0]["switchpoints"][0]["heatSetpoint"]  # type: ignore[call-overload]
+    #
+    # STEP 2: PUT a new schedule
+    temp = schedule["dailySchedules"][0]["switchpoints"][0]["heatSetpoint"]
+    schedule["dailySchedules"][0]["switchpoints"][0]["heatSetpoint"] = temp + 1
 
-    schedule["dailySchedules"][0]["switchpoints"][0]["heatSetpoint"] = temp + 1  # type: ignore[call-overload,operator]
-    result = await should_work_v2(
-        evo.auth,
-        HTTPMethod.PUT,
-        url,
-        json=schedule,  # was convert_to_put_schedule(schedule),  # type: ignore[arg-type]
-    )  # returns (e.g.) {'id': '840367013'}
+    status = await should_work_v2(evo.auth, HTTPMethod.PUT, url, json=schedule)
 
     #
     # STEP 2: check the status of the task
     if _DBG_USE_REAL_AIOHTTP:
-        task_id = result[0]["id"] if isinstance(result, list) else result["id"]
-        url_tsk = f"commTasks?commTaskId={task_id}"
+        task_id = status[0]["id"] if isinstance(status, list) else status["id"]
 
-        status = await should_work_v2(evo.auth, HTTPMethod.GET, url_tsk)
+        status = await should_work_v2(
+            evo.auth, HTTPMethod.GET, f"commTasks?commTaskId={task_id}"
+        )
         # {'commtaskId': '840367013', 'state': 'Created'}
         # {'commtaskId': '840367013', 'state': 'Running'}
         # {'commtaskId': '840367013', 'state': 'Succeeded'}
@@ -86,44 +164,42 @@ async def _test_schedule(evo: EvohomeClientv2) -> None:
     # STEP 3: check the new schedule was effected
     schedule = await should_work_v2(
         evo.auth, HTTPMethod.GET, url, schema=schemas.SCH_GET_SCHEDULE
-    )
-    assert (
-        schedule["dailySchedules"][0]["switchpoints"][0]["heatSetpoint"]  # type: ignore[call-overload]
-        == temp + 1  # type: ignore[operator]
-    )
+    )  # type: ignore[assignment]
+
+    assert schedule["dailySchedules"][0]["switchpoints"][0]["heatSetpoint"] == temp + 1
+    schedule["dailySchedules"][0]["switchpoints"][0]["heatSetpoint"] = temp
 
     #
     # STEP 4: PUT the original schedule back
-    schedule["dailySchedules"][0]["switchpoints"][0]["heatSetpoint"] = temp  # type: ignore[call-overload]
     _ = await should_work_v2(evo.auth, HTTPMethod.PUT, url, json=schedule)
 
     #
-    # STEP 5: check the status of the task
+    # STEP 5: (optional) check the status of the task
 
     #
-    # STEP 6: check the new schedule was effected
+    # STEP 6: check the original schedule was effected
     schedule = await should_work_v2(
         evo.auth, HTTPMethod.GET, url, schema=schemas.SCH_GET_SCHEDULE
-    )
-    assert schedule["dailySchedules"][0]["switchpoints"][0]["heatSetpoint"] == temp  # type: ignore[call-overload]
+    )  # type: ignore[assignment]
 
-    _ = await should_fail_v2(
-        evo.auth, HTTPMethod.PUT, url, json=None, status=HTTPStatus.BAD_REQUEST
-    )  # NOTE: json=None
-
-    #
-    # STEP 7: PUT a bad schedule
-    # [{'code': 'ParameterIsMissing', 'parameterName': 'request', 'message': 'Parameter is missing.'}]
-
-
-# TODO: Test sending bad schedule
+    assert schedule["dailySchedules"][0]["switchpoints"][0]["heatSetpoint"] == temp
 
 
 @skipif_auth_failed  # GET, PUT
-async def test_schedule(evohome_v2: EvohomeClientv2) -> None:
+async def test_schedule_put(evohome_v2: EvohomeClientv2) -> None:
     """Test /{x._TYPE}/{x.id}/schedule
 
-    Also test commTasks?commTaskId={task_id}
+    Does not test /commTasks?commTaskId={task_id}
     """
 
-    await _test_schedule(evohome_v2)
+    await _test_schedule_put(evohome_v2)
+
+
+@skipif_auth_failed  # GET, PUT
+async def test_schedule_tsk(evohome_v2: EvohomeClientv2) -> None:
+    """Test /{x._TYPE}/{x.id}/schedule
+
+    Also tests /commTasks?commTaskId={task_id}
+    """
+
+    await _test_schedule_tsk(evohome_v2)
