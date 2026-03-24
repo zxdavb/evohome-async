3a5f0f3c7cd6348d0ca402252433d2071d329389
David Bonnes <zxdavb@bonnes.me>
2026-03-24 10:55:21 +0000

    tweak exception names

--- src/_evohome/exceptions.py
+++ src/_evohome/exceptions.py
@@ -16,7 +16,9 @@ class EvohomeError(_EvohomeBaseError):
 
 
 # These occur whilst a RESTful API call is being made
-class _ApiRequestFailedError(EvohomeError):
+
+
+class _ApiCallFailedError(EvohomeError):
     """The API request failed for some reason (no/invalid/unexpected response)."""
 
     def __init__(self, message: str, status: int | None = None) -> None:
@@ -24,7 +26,7 @@ class _ApiRequestFailedError(EvohomeError):
         self.status = status  # useful, available if via aiohttp.ClientResponseError
 
 
-class ApiRequestFailedError(_ApiRequestFailedError):  # a base exception, API failed
+class ApiCallFailedError(_ApiCallFailedError):  # a base exception, API failed
     """The API request failed for some reason (no/invalid/unexpected response).
 
     Could be caused by any aiohttp.ClientError, for example: ConnectionError.  If the
@@ -32,11 +34,11 @@ class ApiRequestFailedError(_ApiRequestFailedError):  # a base exception, API fa
     """
 
 
-class ApiRateLimitExceededError(ApiRequestFailedError):
+class ApiRateLimitExceededError(ApiCallFailedError):
     """The API request failed because the vendor's API rate limit was exceeded."""
 
 
-class AuthenticationFailedError(_ApiRequestFailedError):
+class AuthenticationFailedError(_ApiCallFailedError):
     """Unable to authenticate the user credentials (unable to obtain an access token).
 
     The cause could be any ApiRequestFailedError, including RateLimitExceeded.
@@ -50,7 +52,10 @@ class BadUserCredentialsError(AuthenticationFailedError):
     """
 
 
-class BadApiSchemaError(ApiRequestFailedError):  # a base exception, API data bad
+# Request/Response failures (of a RESTful API call)
+
+
+class BadApiSchemaError(ApiCallFailedError):  # a base exception, API data bad
     """The received/supplied JSON is not as expected (e.g. missing a required key)."""
 
 
@@ -62,32 +67,34 @@ class BadApiRequestError(BadApiSchemaError):
     """The supplied parameter(s) are not as expected (e.g. unknown/unsupported mode)."""
 
 
-class InvalidSystemModeError(BadApiRequestError):
+class InvalidSystemModeError(BadApiRequestError):  # failed to set a TCS mode
     """The requested system mode is not supported by this TCS."""
 
 
-class InvalidZoneModeError(BadApiRequestError):
+class InvalidZoneModeError(BadApiRequestError):  # failed to set a zone mode/temperature
     """The requested mode is not supported by this zone."""
 
 
-class InvalidDhwModeError(InvalidZoneModeError):
+class InvalidDhwModeError(InvalidZoneModeError):  # failed to set a DHW zone mode/state
     """The requested mode is not supported by this DHW zone."""
 
 
-class BadScheduleUploadedError(BadApiRequestError):
+class BadScheduleUploadedError(BadApiRequestError):  # failed to set a zone/DHW schedule
     """The supplied schedule JSON is invalid / was not accepted by the vendor."""
 
 
-# These occur without / after a RESTful API call (e.g. a necessary call was not made)
-class _ConfigStatusError(EvohomeError):  # bad/missing data
+# Other, higher failures (after/without a successful API call)
+
+
+class _ConfigStatusError(EvohomeError):  # invalid/missing JSON
     """The config/status JSON is missing or somehow invalid (has it been fetched?)."""
 
 
-class ConfigError(_ConfigStatusError):  # a base exception, bad config JSON
+class ConfigError(_ConfigStatusError):  # account/config JSON is invalid/missing
     """The config JSON is missing or somehow invalid (e.g. InvalidSchemaError)."""
 
 
-class InvalidConfigError(ConfigError):
+class InvalidConfigError(ConfigError):  # account/config JSON is invalid/missing
     """The system config JSON is missing/invalid (has it been fetched?).
 
     This is likely because the user has not yet been authenticated (or authentication
@@ -99,18 +106,18 @@ class NoSingleTcsError(ConfigError):
     """There is no default TCS (e.g. the user has more than one location)."""
 
 
-class StatusError(_ConfigStatusError):  # a base exception, bad status/schedule JSON
+class StatusError(_ConfigStatusError):  # status/schedule JSON is invalid/missing
     """The status JSON is missing or somehow invalid (e.g. BadApiResponseSchemaError)."""
 
 
-class InvalidStatusError(StatusError):
+class InvalidStatusError(StatusError):  # status JSON is invalid/missing
     """The status JSON is missing/invalid (has it been fetched?).
 
     This is likely because the user has not yet called `Location.update()`.
     """
 
 
-class InvalidScheduleError(StatusError):
+class InvalidScheduleError(StatusError):  # schedule JSON is invalid/missing
     """The schedule JSON is missing/invalid (has it been fetched?).
 
     This is likely because the user has not yet called `Zone.get_schedule()`.
