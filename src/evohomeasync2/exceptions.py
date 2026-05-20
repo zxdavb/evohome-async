3529072868242cf5c2c2b780aefdf4eb16a8c254
David Bonnes <zxdavb@bonnes.me>
2026-05-20 23:05:27 +0100

    fix: make backward-compatible deprecation for new exception names

--- src/evohomeasync2/exceptions.py
+++ src/evohomeasync2/exceptions.py
@@ -5,6 +5,7 @@ from __future__ import annotations
 from _evohome.exceptions import (
     ApiCallFailedError,
     ApiRateLimitExceededError,
+    ApiRequestFailedError,
     AuthenticationFailedError,
     BadApiRequestError,
     BadApiResponseError,
@@ -26,6 +27,7 @@ from _evohome.exceptions import (
 __all__ = [
     "ApiCallFailedError",
     "ApiRateLimitExceededError",
+    "ApiRequestFailedError",  # deprecated alias for ApiCallFailedError
     "AuthenticationFailedError",
     "BadApiRequestError",
     "BadApiResponseError",
